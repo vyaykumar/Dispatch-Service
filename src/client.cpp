8845a58db764fe314952a4dd3b7e55f7a59dd074
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-07-29 22:44:43 +0530

    First round of Refactoring done.

--- src/client.cpp
+++ src/client.cpp
@@ -2,78 +2,180 @@
 #include "protocol.h"
 #include "transport.h"
 
+// Refactoring Begins.
+
 namespace {
+    constexpr DWORD timeoutMs = 5000; // for Windows
     constexpr uint16_t kPort = 50051;
 
+    // Following 4 are to be a struct that is passed around.
+    transport::socket_t sock {};
+    sockaddr_in serv_addr{};
+    std::optional<protocol::DecodedMessage> ackMsg, resultMsg; // Kept as optional now. Maybe `expected` later.
+
+    enum class DispatchError {
+        SockCreation_Failure,
+        ServAddress_Failure,
+        TimeoutConf_Failure,
+        Conn_Failure,
+        TaskSub_Failure,
+        ACK_Failure,
+        TimeOut,
+        Receive_Failure,
+        Unexpected_Message
+    };
+}
+
+
+// Socket Initialization.
+[[nodiscard]] std::expected<void, DispatchError>
+initSock () {
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    // THis can fail. But where to put the check?
+    // Maybe return a bool, but that will need a check too?
+    // Check here instead of cluttering the disp_func
+
+    if (sock == transport::kInvalidSocket)
+        // std::cout << "[dispatcher] socket creation failed\n";
+        // Output will be handled in main();
+        return std::unexpected(DispatchError::SockCreation_Failure);
+
+    std::cout << "[dispatch]: Socket initialized.\n";
+    return {};
+}
+
+// Server Address.
+[[nodiscard]] std::expected<void, DispatchError>
+initServAddr () {
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(kPort);
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+
+    // Forgot the error code for this.
+    // if (serv_addr == transport::) {
+    //     return true;
+    // }
+
+    // There are no failure states here.
+    std::cout << "[dispatch]: Server Address initialized.\n";
+    return {};
+}
+
+[[nodiscard]] std::expected <void, DispatchError>
+initTimeOut () {
+    const auto flag = setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO,
+        reinterpret_cast<const char *>(&timeoutMs), sizeof(timeoutMs));
+
+    if (flag != 0)
+        return std::unexpected(DispatchError::TimeoutConf_Failure);
+
+    std::cout << "[dispatch]: Receive_timeout set.\n";
+    return {};
+}
+
+[[nodiscard]] std::expected <void, DispatchError>
+startConn() {
+    if (connect(sock, reinterpret_cast<sockaddr*>(&serv_addr), sizeof(serv_addr)) != 0)
+        return std::unexpected(DispatchError::Conn_Failure);
+
+    std::cout << "[dispatch]: Connection established.\n";
+    return {};
+}
+
+[[nodiscard]] std::expected<void, DispatchError>
+submitTask () {
+    const protocol::TaskSubmit submit{
+        .taskId = "task-0001",
+        .idempotencyKey = "idem-key-abc",
+        .payload = std::vector<uint8_t>{'h', 'e', 'l', 'l', 'o'}
+    };
+
+    if (!protocol::SendTaskSubmit(sock, submit))
+        return std::unexpected(DispatchError::TaskSub_Failure);
+
+    std::cout << "[dispatch]: Task submitted successfully.\n";
+    return {};
+}
+
+[[nodiscard]] std::expected<void, DispatchError>
+recAck () {
+    // Do we propagate std::expected to receiveMessage too?
+    // Change type in the namespace, if yes.
+    ackMsg = protocol::ReceiveMessage(sock);
+    if (!ackMsg or ackMsg->type != protocol::MessageType::kTaskAck)
+        // Should we return separate error enums for non-arrival, and unexpected arrival?
+        return std::unexpected(DispatchError::ACK_Failure);
+
+    std::cout << "[dispatcher]: TaskID(" << ackMsg->ack.taskId << ") acknowledged.\n";
+    return {};
+}
+
+[[nodiscard]] std::expected<protocol::TaskResult, DispatchError>
+recMes () {
+    resultMsg = protocol::ReceiveMessage(sock);
+
+    // Check if Message is empty.
+    if (!resultMsg) {
+        // Cause of timeout.
+        if (WSAGetLastError() == WSAETIMEDOUT) {
+            std::cout << "[dispatcher]: Connection timed Out.\n";
+            return std::unexpected(DispatchError::TimeOut);
+        }
+        // Any other reason.
+        std::cout << "[dispatcher]: Failed to receive message.\n";
+        return std::unexpected(DispatchError::Receive_Failure);
+    }
+
+    if (resultMsg->type != protocol::MessageType::kTaskResult) {
+        std::cout << "[dispatcher]: Expected TASK_RESULT. Received else.\n";
+        return std::unexpected(DispatchError::Unexpected_Message);
+    }
+
+    const std::string resultText(resultMsg->result.payload.begin(), resultMsg->result.payload.end());
+    std::cout << "[dispatcher]: received TASK_RESULT: status("
+              << static_cast<int>(resultMsg->result.status)
+              << "). Payload: \"" << resultText << "\"\n";
+    return {};
+}
+
+// Placeholder. Replace Appropriately.
+using Result = std::string;
+
+[[nodiscard]] std::expected<Result, DispatchError>
+dispatch () {
+    if (auto result = initSock(); !result)
+        return std::unexpected(result.error());
+
+    if (auto result = initServAddr(); !result)
+        return std::unexpected(result.error());
+
+    if (auto result = initTimeOut(); !result)
+        return std::unexpected(result.error());
+
+    if (auto result = startConn(); !result)
+        return std::unexpected(result.error());
+
+    if (auto result = submitTask(); !result)
+        return std::unexpected(result.error());
+
+    if (auto result = recAck(); !result)
+        return std::unexpected(result.error());
+
+    if (auto result = recMes(); !result)
+        return std::unexpected(result.error());
+
+    return "PlaceHolder.\n";
 }
 
 int main() {
     std::cout << "Client starting\n";
     transport::PlatformInit();
 
-    transport::socket_t sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == transport::kInvalidSocket) {
-        std::cout << "[dispatcher] socket creation failed\n";
-        return 1;
-    }
+    auto failure = dispatch();
+    // Switch for the failure states.
 
-    sockaddr_in serverAddr{};
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(kPort);
-    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
-
-    // POSIX stuff.
-    // struct timeval timeout;
-    // timeout.tv_sec = 5; //seconds
-    // timeout.tv_usec = 0;//microseconds
-
-    DWORD timeoutMs = 5000; //Windows
-    auto flag = setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, reinterpret_cast<const char *>(&timeoutMs), sizeof(timeoutMs));
-    if (flag != 0) {
-        std::cout << "[dispatcher] failed to set receive timeout\n";
-    }
-
-    if (connect(sock, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) != 0) {
-        std::cout << "[dispatcher] connect failed\n";
-        return 1;
-    }
-
-    protocol::TaskSubmit submit{
-        "task-0001",
-        "idem-key-abc",
-        std::vector<uint8_t>{'h', 'e', 'l', 'l', 'o'}
-    };
-    protocol::SendTaskSubmit(sock, submit);
-    std::cout << "[dispatcher] sent TASK_SUBMIT\n";
-
-    auto ackMsg = protocol::ReceiveMessage(sock);
-    if (!ackMsg or ackMsg->type != protocol::MessageType::kTaskAck) {
-        std::cout << "[dispatcher] Did not receive expected TASK_ACK\n";
-        return 1;
-    }
-    std::cout << "[dispatcher] received TASK_ACK for task_id=" << ackMsg->ack.taskId << "\n";
-
-    auto resultMsg = protocol::ReceiveMessage(sock);
-
-    if (!resultMsg) {
-        std::cout << "WSA Error: "
-                  << WSAGetLastError()
-                  << '\n';
-        return 1;
-    }
-
-    if (resultMsg->type != protocol::MessageType::kTaskResult) {
-        std::cout << "[dispatcher] did not receive expected TASK_RESULT\n";
-        return 1;
-    }
-    std::string resultText(resultMsg->result.payload.begin(), resultMsg->result.payload.end());
-    std::cout << "[dispatcher] received TASK_RESULT: status="
-              << static_cast<int>(resultMsg->result.status)
-              << " payload=\"" << resultText << "\"\n";
-
-    std::cout << "Protocol OK.\n";
+    std::cout << "Protocol works.\n";
 
     transport::CloseSocket(sock);
-
     transport::PlatformCleanup();
 }
