1b883754f7632ddfa8567d6a3079a7f386e23785
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-08-03 17:38:54 +0530

    HandleConnection refactor is complete.

--- src/by_my_hand.cpp
+++ src/by_my_hand.cpp
@@ -21,7 +21,7 @@ namespace {
 
     // Functions yonder. void* is a placeholder.
 
-    std::expected<protocol::DecodedMessage, ErrorStates> ReceiveSubmit (const socket_t client) {
+    std::expected<protocol::DecodedMessage, ErrorStates> ReceiveMessage (const socket_t client) {
         auto message = protocol::ReceiveMessage(client);
 
         // Malformed Message
@@ -40,13 +40,27 @@ namespace {
     }
 
     // 2. void* recSub                - Protocol Handshake: Send the ACK.
-    std::expected <void, ErrorStates> receive_submit (const socket_t client, const protocol::TaskId& taskID) {
+    std::expected <void, ErrorStates> SendACK (const socket_t client, const protocol::TaskId& taskID) {
         // Quick return, in case of failure.
         if (!protocol::SendTaskAck(client, {taskID})) {
             std::println ("[worker]: ACK Failure. Rejected.");
             return std::unexpected(ErrorStates::ACKFailure);
         }
         std::println("[worker]: ACK sent successfully.");
+        return {};
+    }
+
+    // Monadic chain function
+    std::expected<protocol::DecodedMessage, ErrorStates> SubmitACK(const socket_t client, protocol::DecodedMessage message)
+    {
+        return SendACK(
+            client,
+            message.submit.taskId
+        )
+        .transform([msg = std::move(message)]() mutable
+        {
+            return std::move(msg);
+        });
     }
 
     protocol::TaskResult work(const protocol::TaskId &taskID) {
@@ -67,7 +81,7 @@ namespace {
         return std::move(res);
     }
 
-    std::expected<protocol::TaskResult, ErrorStates> exec (const protocol::TaskId& taskID) {
+    std::expected<protocol::TaskResult, ErrorStates> Process (const protocol::TaskId& taskID) {
         task_registry::TaskRegistry task_reg;
         using Action = task_registry::Action;
 
@@ -76,25 +90,38 @@ namespace {
 
         // `action` tells us what to do.
         switch (auto [action, result] = task_reg.try_claim(taskID); action) {
-            case Action::Reject  : return std::unexpected (ErrorStates::JobExists); break;
-            case Action::Execute : return std::move (work(taskID));                 break;
-            case Action::Cached  : return std::move (result.value());               break;
-            default              : std::println("Don't know how you got here.\n");  break;
+            case Action::Reject  : return std::unexpected (ErrorStates::JobExists);
+            case Action::Execute : return std::move (work(taskID));
+            case Action::Cached  : return std::move (result.value());
         }
     }
 
     // 5. void* SendResult            - Response: Build and send the TASK_RESULT, close the socket, flip done.
 
-
-
+    std::expected<void, ErrorStates> SendResult (const socket_t client, const protocol::TaskResult& result) {
+        if (protocol::SendTaskResult(client, result))
+            std::println ("[worker]: Result successfully sent.");
+        else
+            std::println ("[worker]: Result for taskID(",result.taskId,") couldn't be sent.");
+        std::println("[worker]: Terminating.");
+    }
 
 
     void HandleConnection(std::stop_token stopToken, const socket_t client, std::atomic_bool& done) {
-        std::cout << "[worker]: Thread started.\n";
-
+        // Preprocessor RAII magic.
         defer(transport::CloseSocket(client));
         defer(done.store(true));
 
-    }
+        // Function starts here.
+        std::println("[worker]: Thread started.");
 
+        const auto message = ReceiveMessage(client).and_then(std::bind_front(SubmitACK, std::ref(client)));
+        if (!message)
+            return;
+
+        if (const auto result = Process(message.value().submit.taskId).and_then(std::bind_front(SendResult, client)); !result)
+            return;
+
+        std::println("Thread finished.");
+    }
 }
