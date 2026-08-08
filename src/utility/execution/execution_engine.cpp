367efe0b9724f900d48b2a033a55fcd64fe953df
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-08-08 19:20:37 +0530

    Task N+7: ExEn v0.2, basic functionality. Doesn't work.

--- src/utility/execution/execution_engine.cpp
+++ src/utility/execution/execution_engine.cpp
@@ -5,18 +5,17 @@
 #include "execution_engine.h"
 
 namespace execution {
-    ExecutionResult ExecuteScenario (const scenario::Config& config,
-    const std::string& serverAddr,
-        uint16_t port) {
-        ExecutionResult result {};
+    ExecutionResult ExecuteScenario (const scenario::Config& config) {
+
+        ExecutionResult res_vec;
 
         // Parse the config.
         auto [scene,work_conf,clients,stagger] = config;
         // Spawn N clients, with stagger time between them.
         while (clients--) {
-            //Spawn 1 client.
-            //Sleep for stagger seconds. Make the timekeeping absolute.
-            // Collect Results.
+            Result res = client::RunClient(ctx);
+            res_vec.results.push_back(std::move(res));
+            std::this_thread::sleep_for(stagger);
         }
         // Return aggregated results.
     }
--- src/utility/execution/execution_engine.h
+++ src/utility/execution/execution_engine.h
@@ -2,21 +2,14 @@
 
 #include <bits/stdc++.h>
 #include "../scenario/scenarios.h"
+#include "../client/client_interface.h"
 
 namespace execution {
-    struct ClientContext {
-        sockaddr_in serv_addr{};
-    };
+    client::cli_ctx;
 
     struct ExecutionResult {
-        int successCount;
-        int failureCount;
-        std::vector<std::string> errors;
+        std::vector<Result> results;
     };
 
-    ExecutionResult ExecuteScenario(
-        const scenario::Config& config,
-        const std::string& serverAddr,
-        uint16_t port
-    );
+    ExecutionResult ExecuteScenario(const scenario::Config& config);
 }
