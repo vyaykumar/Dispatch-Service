5779a881d4d3f3f7e1ae193adc9d7904f0ba4b44
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-08-10 09:32:46 +0530

    WorkerPool 0.1 : Spinning out worker_interface.

---
+++ src/utility/worker_pool/worker_pool.cpp
@@ -0,0 +1,5 @@
+//
+// Created by VijayKumar on 10-08-2026.
+//
+
+#include "worker_pool.h"
---
+++ src/utility/worker_pool/worker_pool.h
@@ -0,0 +1,45 @@
+#ifndef DISPATCH_SERVICE_SMOKETEST_WORKER_POOL_H
+#define DISPATCH_SERVICE_SMOKETEST_WORKER_POOL_H
+
+#include <condition_variable>
+#include <queue>
+#include <thread>
+
+#include "../Wire/transport.h"
+
+namespace worker_pool {
+    struct Item {
+        transport::socket_t socket;
+    };
+
+    enum class SpeedClass {
+        Fast,
+        Normal,
+        Slow
+    };
+
+    struct Profile {
+        SpeedClass speed;
+        double speed_factor;
+    };
+
+    struct Worker {
+        Profile profile;
+        std::jthread thread;
+    };
+
+    class WorkerPool {
+    public:
+        explicit WorkerPool (size_t worker_count);
+        void Enqueue (Item item);
+
+    private:
+        std::vector<Worker> workers_;
+        std::queue<Item> queue_;
+        std::mutex mutex_;
+        std::condition_variable condition_variable_;
+    };
+}
+
+
+#endif //DISPATCH_SERVICE_SMOKETEST_WORKER_POOL_H
