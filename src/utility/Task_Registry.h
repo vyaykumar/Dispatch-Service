1947b7f3920938cef4304e6de8487685a85463aa
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-08-12 11:18:56 +0530

    Refactor 0.8 : Finished with Task Registry.

--- src/utility/Task_Registry.h
+++ src/utility/Task_Registry.h
@@ -25,35 +25,35 @@ namespace task_registry {
     public:
         struct TaskRecord {
             ExecutionStatus status {};
-            std::optional<protocol::TaskResult> result {};
+            std::optional<protocol::TaskResult> t_result {};
         };
 
         struct Result {
             Action action {};
-            std::optional<protocol::TaskResult> result {};
+            std::optional<protocol::TaskResult> t_result {};
         };
 
-        Result tryClaim (const protocol::T_ID &id) {
+        Result tryClaim (const protocol::T_ID &task_id) {
             std::scoped_lock lock (mutex_);
 
             // Check if it exists. And it does.
-            if (const auto entry_it = task_table_.find(id); entry_it != task_table_.end()) {
+            if (const auto entry_it = task_table_.find(task_id); entry_it != task_table_.end()) {
                 if (entry_it->second.status == ExecutionStatus::Completed)
-                    return {.action = Action::Cached, .result = entry_it->second.result};
+                    return {.action = Action::Cached, .t_result = entry_it->second.t_result};
 
                 // It has to be Processing, then.
                 return {.action = Action::Reject};
             }
 
             // It doesnt exist.
-            task_table_[id] = {.status = ExecutionStatus::Processing};
+            task_table_[task_id] = {.status = ExecutionStatus::Processing};
             return {.action = Action::Execute};
         }
 
-        void markComplete (const protocol::T_ID &id, protocol::TaskResult task) {
+        void markComplete (const protocol::T_ID &task_id, protocol::TaskResult task_result) {
             std::scoped_lock lock (mutex_);
 
-            const auto it = task_table_.find(id);
+            const auto it = task_table_.find(task_id);
 
             if (it == task_table_.end() or it->second.status != ExecutionStatus::Processing) {
                 std::cout << "Function mark_complete called on non-Processing task.\nTerminating.\n";
@@ -61,7 +61,7 @@ namespace task_registry {
             }
 
             it->second.status = ExecutionStatus::Completed;
-            it->second.result = std::move(task);
+            it->second.t_result = std::move(task_result);
         }
 
     private:
